cb9ff86f792508ed584eb71d7cfadc9be9ecf4cd
qwiklabs project <student-03-696324bb0066@qwiklabs.net>
2021-04-30 19:17:16 +0700

    Add transaction API

--- config.py
+++ config.py
@@ -3,14 +3,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    HOST = str(os.environ.get("DB_HOST"))
-    DATABASE = str(os.environ.get("DB_DATABASE"))
-    USERNAME = str(os.environ.get("DB_USERNAME"))
-    PASSWORD = str(os.environ.get("DB_PASSWORD"))
+    ## using MySQL
+    # HOST = str(os.environ.get("DB_HOST"))
+    # DATABASE = str(os.environ.get("DB_DATABASE"))
+    # USERNAME = str(os.environ.get("DB_USERNAME"))
+    # PASSWORD = str(os.environ.get("DB_PASSWORD"))
 
+    JSON_SORT_KEYS = False
 
     JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET"))
     
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
+    ## using MySQL
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
